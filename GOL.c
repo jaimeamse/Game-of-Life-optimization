bfe88b042d0995e921e145b3fd08feb61e5a7064
CatCodeMage <andreupuigvert0013@gmail.com>
2025-11-18 10:09:17 +0100

    Evaluete Cell 2
    aa-26@aolin-login proyecto]$ make time
    gcc -Ofast -march=native -fopenmp ./GOL.c -o GOL
    perf stat ./GOL 1000
    Matrix is 10000 x 10000.
    Number of iterations: 1000
    CheckSum = 2181762
    
     Performance counter stats for './GOL 1000':
    
             46.179,73 msec task-clock:u              #    5,904 CPUs utilized          
                     0      context-switches:u        #    0,000 /sec                   
                     0      cpu-migrations:u          #    0,000 /sec                   
                 3.957      page-faults:u             #   85,687 /sec                   
       183.064.620.279      cpu_core/cycles:u/        #    3,964 G/sec                  
       292.413.016.145      cpu_core/instructions:u/  #    6,332 G/sec                  
         3.274.339.129      cpu_core/branches:u/      #   70,904 M/sec                  
            10.103.376      cpu_core/branch-misses:u/ #  218,784 K/sec                  
    
           7,822151777 seconds time elapsed
    
          45,926248000 seconds user
           0,047882000 seconds sys

--- GOL.c
+++ GOL.c
@@ -5,95 +5,155 @@
 #define X 10000
 #define Y 10000
 
-static unsigned char seed;
+static unsigned int seed;
 
-static inline char myRandom() {
+static inline unsigned char myRandom() {
   seed = (214013*seed+2531011);
   return (seed>>16) & 1;
 }
 
-// Coloca valores aleatorios
-void inicializar_tablero ( char T[Y+2][X+2] )
-{
-  int i, j;
-  for ( i= 0; i < X+2; i++ )
-    for ( j= 0; j < Y+2; j++ )
-      T[i][j] = myRandom();
+static inline unsigned char updateSeed() {
+  seed = (214013*seed+2531011);
 }
 
-void ajustar_marco( char T[Y+2][X+2] )
+// Coloca valores aleatorios
+void __attribute__((noinline)) inicializar_tablero ( unsigned char T[Y+2][X+2] )
+{
+  int i, j;
+  unsigned copy = seed;
+  //sumVal = 406518922;
+  //mulVal = 1080739145;
+  for ( i= 0; i < Y+2; i++ ) {
+    copy = (214013*copy+2531011);
+    seed = copy;
+    for ( j= 0; j < X+2; j++ ) {
+      T[i][j] = (seed>>16) & 1;
+      seed = (1080739145*seed+406518922);
+    }
+  }
+}
+
+void ajustar_marco( unsigned char T[Y+2][X+2] )
 {
   int i;
   for ( i=0; i < X+2; i++ )  T[0][i] = T[Y+1][i] = 0;
   for ( i=0; i < Y+2; i++ )  T[i][0] = T[i][X+1] = 0;
 }
 
-void copiar_tablero ( char in[Y+2][X+2], char out[Y+2][X+2] )
+void __attribute__((noinline)) copiar_tablero ( unsigned char in[Y+2][X+2], unsigned char out[Y+2][X+2] )
 {
   int i,j;
-  
-  for ( i=0; i < X+2; i++ )
-    for ( j=0; j < Y+2; j++ )
+  for ( i=0; i < Y+2; i++ )
+    for ( j=0; j < X+2; j++ )
       out[i][j] = in[i][j];
 }
 
-
-int evaluar_celda ( char tablero[Y+2][X+2], int fila, int columna )
+int evaluar_celda ( unsigned char tablero[Y+2][X+2], int fila, int columna ) // VECTORIZADO
 {
-  int vecinos, i, j;
-  //int YO =  tablero[fila][columna];
-
-  vecinos = -tablero[fila][columna];
+  int vecinos= -(tablero[fila][columna]);
+  int i, j;
+ 
   // contar celdas vivas (con un 1) en la vecindad del elemento (fila,columna) 
   //   incluyendo a la celda central
-  for ( i= -1 ; i < 2 ; i++ )
-    for ( j= -1 ; j < 2 ; j++ )
-        vecinos += tablero[fila+i][columna+j];
+  for ( j= -1 ; j < 2 ; j++ )
+    for ( i= -1 ; i < 2 ; i++ )
+      vecinos+= tablero[fila+i][columna+j];
 
-  return (vecinos == 2 || vecinos == 3);
+  return ( ( vecinos == 2 && (tablero[fila][columna]) ) || vecinos == 3) ;
 }
 
+int evaluar_celda_2 ( unsigned char tablero[Y+2][X+2], int fila, int columna ) // VECTORIZADO 
+{
+  int vecinos = ((tablero[fila-1][columna-1] + tablero[fila-1][columna]) 
+                  + (tablero[fila-1][columna+1] + tablero[fila][columna-1]) ) 
+                  + ( (tablero[fila][columna+1] + tablero[fila+1][columna-1]) 
+                  + (tablero[fila+1][columna] + tablero[fila+1][columna+1]));
+    
+  int celda = tablero[fila][columna];
+  return (vecinos == 3) || (vecinos == 2 && celda);
+}
+
+
 // Tablero del Juego de la VIDA
-static char tablero[Y+2][X+2];
+static unsigned char tablero[Y+2][X+2];
 
 // Se usa para preservar valores durante el computo
-static char tablero_auxiliar[Y+2][X+2];
+static unsigned char tablero_auxiliar[Y+2][X+2];
 
-int actualizar_tablero ( char tablero_original[Y+2][X+2] )
+void __attribute__((noinline)) actualizar_tablero ( unsigned char  tablero_original[Y+2][X+2], unsigned char tablero_tmp[Y+2][X+2] )
 {
-  int count= 0, old, new, i,j;
+  int i,j;
 
-  copiar_tablero ( tablero_original, tablero_auxiliar );
-  
-  
-  for ( i=1; i < X+1; i++ )
-    for ( j=1; j < Y+1; j++ )
+  //copiar_tablero ( tablero_original, tablero_tmp );
+  #pragma omp for 
+  for ( i=1; i < Y+1; i++ )
+    for ( j=1; j < X+1; j++ )
     {
-      old = tablero_auxiliar[i][j];
-      new = evaluar_celda (tablero_auxiliar, i, j);
-      tablero_original[i][j] = new;
-      count += old != new;
+      tablero_tmp[i][j] = evaluar_celda_2 (tablero_original, i, j);
+    }
+}
+
+
+int __attribute__((noinline)) actualizar_tablero_end ( unsigned char tablero_original[Y+2][X+2])
+{
+  int count= 0, old, neW, i,j;
+
+  for ( i=1; i < Y+1; i++ )
+    for ( j=1; j < X+1; j++ )
+    {
+      old = tablero_original[i][j];
+      neW = evaluar_celda_2 (tablero_original, i, j);
+      count+=(old != neW); 
     }
   return count; // numero de celdas que han cambiado
 }
 
+
 int JuegoDeLaVida ( int time )
 {
-  int cambios;
-
+  /*
+  unsigned int mulVal = 1;
+  unsigned int sumVal = 0;
+  unsigned copy = seed;
+  for (int i = 0; i < Y+2; i++)
+  {
+    sumVal = sumVal + mulVal*2531011;
+    mulVal *= 214013;
+  }*/
+    
+  //sumVal = 406518922;
+  //mulVal = 1080739145;
+  int checkSum;
   // inicializar de forma aleatoria
   inicializar_tablero ( tablero );
   ajustar_marco       ( tablero );
-
-  while (time > 0)
+  ajustar_marco       ( tablero_auxiliar );
+  
+  #pragma omp parallel shared(checkSum, tablero, tablero_auxiliar, time) num_threads(6)
   {
-    cambios = actualizar_tablero(tablero);
-    time = time - 1;
+    while (time > 2)
+    {
+      actualizar_tablero(tablero, tablero_auxiliar);
+      #pragma omp barrier
+      actualizar_tablero(tablero_auxiliar, tablero);
+      #pragma omp master 
+      time = time - 2;
+      #pragma omp barrier
+    }
+    if (time == 2) {
+      actualizar_tablero(tablero, tablero_auxiliar);
+    }
   }
-  return cambios;
-}
 
-int main (int argc, char **argv)
+  if (time == 2) 
+    checkSum = actualizar_tablero_end(tablero_auxiliar);
+  else 
+    checkSum = actualizar_tablero_end(tablero);
+  
+  return checkSum;
+} 
+
+int main (int argc, unsigned char **argv)
 {
   int time_steps = 30; // Default number of time steps
 
@@ -105,7 +165,7 @@ int main (int argc, char **argv)
 
   printf( "Matrix is %d x %d.\nNumber of iterations: %d\n", Y, X, time_steps);
 
-  int chk = JuegoDeLaVida ( time_steps );
+  int chk = JuegoDeLaVida ( time_steps );  
 
   printf("CheckSum = %d\n", chk);
 
